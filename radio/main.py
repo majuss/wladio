80e6f0502854547ade24222c78ae421358e25810
majuss <biomajuss@gmail.com>
2020-03-19 20:16:11 +0000

    further init

---
+++ radio/main.py
@@ -0,0 +1,3 @@
+import mpv
+player = mpv.MPV(ytdl=True)
+player.play('http://st01.dlf.de/dlf/01/128/mp3/stream.mp3')
---
+++ setup.sh
@@ -0,0 +1,8 @@
+# Linux packages
+sudo apt install libmpv1
+
+
+# Python venv
+python3 -m venv venv
+source venv/bin/activate
+pip install python-mpv
