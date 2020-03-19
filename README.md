f845f1b38fb1200f74ebda9a634e7f78b488ad85
majuss <biomajuss@gmail.com>
2020-03-19 20:27:14 +0000

    further init

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # wladio
 Building a WIFI radio based on a Raspberry Pi
+
+
+
+## Audio
+
+## Infrared
+
+Links:
+- https://indibit.de/raspberry-pi-mit-apple-remotelirc-python-scripte-steuern/
+
+## RFID
+
+Links:
+https://codingworld.io/project/rfid-grundlagen
+
+## Housing
+
+Max size is 50x25x15 cm
--- setup.sh
+++ setup.sh
@@ -1,8 +1,17 @@
 # Linux packages
 sudo apt install libmpv1
 
+# For IR
+sudo apt install lirc python-pylirc --yes
+sudo modprobe lirc_dev
+sudo modprobe lirc_rpi gpio_in_pin=17 gpio_out_pin=23
+
+
+# sudo nano /boot/config.txt
+# dtoverlay=lirc-rpi,gpio_in_pin=17,gpio_out_pin=23
 
 # Python venv
 python3 -m venv venv
 source venv/bin/activate
 pip install python-mpv
+pip install luma.oled
