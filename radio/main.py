1cd7ec50daff400c44321c758aed8dc0ef62accb
majuss <biomajuss@gmail.com>
2020-03-31 16:10:55 +0100

    make audio streaming more modular

--- radio/main.py
+++ radio/main.py
@@ -1,16 +1,25 @@
 import mpv
 import json
 
+# Load stations file
 with open('stations.json') as stations_file:
   stations = json.load(stations_file)
 
+current_station = ''
+
+
 def stream_logger(loglevel, component, message):
-    if message.lstrip() not in stations['fritz']['skip_strings'] and "icy-title" in message:
-        for replace_string in stations['fritz']['replace_strings']:
+    global current_station
+    if message.lstrip() not in stations[current_station]['skip_strings'] and "icy-title" in message:
+        for replace_string in stations[current_station]['replace_strings']:
             message = message.replace(replace_string, "")
         print(message.lstrip())
-#replace Jetzt läuft
 
-player = mpv.MPV(log_handler=stream_logger)
-player.play(stations['fritz']['url'])
-player.wait_for_playback()
+def play_stream(stream_name):
+    global current_station
+    current_station = stream_name
+    player = mpv.MPV(log_handler=stream_logger)
+    player.play(stations[stream_name]['url'])
+    player.wait_for_playback()
+
+play_stream('fritz')
