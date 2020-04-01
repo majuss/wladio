76ee56f7446492af9e0a61ef3ee0247fe895190c
majuss <biomajuss@gmail.com>
2020-04-01 16:59:58 +0100

    forked player to thread; real icy tag handling via player metadata

--- radio/main.py
+++ radio/main.py
@@ -1,25 +1,37 @@
 import mpv
 import json
+import threading
+import time
 
 # Load stations file
 with open('stations.json') as stations_file:
   stations = json.load(stations_file)
 
 current_station = ''
-
-
-def stream_logger(loglevel, component, message):
-    global current_station
-    if message.lstrip() not in stations[current_station]['skip_strings'] and "icy-title" in message:
-        for replace_string in stations[current_station]['replace_strings']:
-            message = message.replace(replace_string, "")
-        print(message.lstrip())
+player = ''
 
 def play_stream(stream_name):
     global current_station
+    global player
     current_station = stream_name
-    player = mpv.MPV(log_handler=stream_logger)
+    player = mpv.MPV()
     player.play(stations[stream_name]['url'])
     player.wait_for_playback()
 
-play_stream('fritz')
+player_thread = threading.Thread(target=play_stream, args=('fritz',))
+player_thread.start()
+while not hasattr(player, "metadata"):
+    time.sleep(1)
+while not "icy-title" in player.metadata:
+    time.sleep(1)
+
+last_tag = ''
+while True:
+    tag = player.metadata['icy-title']
+
+    if last_tag != player.metadata['icy-title'] and player.metadata['icy-title'] not in stations[current_station]['skip_strings']:
+        for replace_string in stations[current_station]['replace_strings']:
+            tag = tag.replace(replace_string, '').lstrip()
+        print(tag)
+        last_tag = player.metadata['icy-title']
+    time.sleep(1)
