65fb3a4c0464a63f610f4da4541ecf542e0a8958
majuss <biomajuss@gmail.com>
2020-05-18 18:14:52 +0100

    revert

--- display/main.py
+++ display/main.py
@@ -1,13 +1,11 @@
 from luma.core.interface.serial import spi
 from luma.core.render import canvas
-from luma.core.virtual import viewport
-
 from luma.oled.device import ssd1322
 from time import sleep
 
 import math
 from PIL import ImageFont
-main_font = ImageFont.truetype("../radio/fonts/hel_new.otf",68) # 68 max # min 26
+main_font = ImageFont.truetype("../radio/fonts/hel_new.otf",25) # 68 max # min 25
 
 # rev.1 users set port=0
 # substitute spi(device=0, port=0) below if using that interface
@@ -17,40 +15,17 @@ serial = spi(device=0, port=0)
 # substitute ssd1331(...) or sh1106(...) below if using that device
 device = ssd1322(serial)
 
-stringie = 'Deine Mutter Lorem Ipsum'
+stringie = 'wwwwwwd'
 
-# font_size = math.floor(-20.226 * math.log(len(stringie)) + 68.882)
-
-font_size = 68
+font_size = math.floor(-20.226 * math.log(len(stringie)) + 68.882)
+print(font_size)
 main_font = ImageFont.truetype("../radio/fonts/hel_new.otf", font_size)
 
-
-while main_font.getsize(stringie)[0] > 256:
-    font_size -= 1
-    main_font = ImageFont.truetype("../radio/fonts/hel_new.otf", font_size)
-    if font_size == 25:
-        break
-
-
-string_size = main_font.getsize(stringie)
-print(string_size[1])
-ycursor = math.ceil((64 - string_size[1])/2 +1)
-print(ycursor)
-
-virtual = viewport(device, width=max(device.width, string_size[0]), height=device.height)
-
 while True:
-
-    with canvas(virtual) as draw:
-        draw.text((0, ycursor), stringie, fill="white", font=main_font) 
-    i = 0
-    sleep(0.5)
-
-    while i < string_size[0] -  device.width :
-        virtual.set_position((i, 0))
-        if i == 0:
-            sleep(0.5)
-        i += 1
-        sleep(0.03333)
+    with canvas(device) as draw:
+        # draw.rectangle((0, 0, 64, 64), outline="white", fill="white") #xy xy
+        draw.text((0,0),stringie, fill="white", font=main_font) 
+        # sleep(5)
+    # draw.text((30, 40), "Hello World", fill="white")
 
 
