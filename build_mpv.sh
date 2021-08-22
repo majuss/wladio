5d2f234e3dfb7e7bbbf74630c999a55dbf0349ff
Manuel B <baslr@users.noreply.github.com>
2021-08-22 22:50:18 +0200

    Update build_mpv.sh

--- build_mpv.sh
+++ build_mpv.sh
@@ -4,6 +4,7 @@ git clone https://github.com/mpv-player/mpv
 
 cd mpv
 git checkout master
+ ./bootstrap.py
 ./waf configure --enable-libmpv-shared
 ./waf build --enable-libmpv-shared
 sudo ./waf install --enable-libmpv-shared
