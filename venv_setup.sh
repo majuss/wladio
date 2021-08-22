2a08efa9a1baf66962fa9f5d41744ebcf7129f1d
majuss <biomajuss@gmail.com>
2021-08-22 21:26:19 +0200

    Update venv_setup.sh

--- venv_setup.sh
+++ venv_setup.sh
@@ -1,5 +1,5 @@
 rm -rf /home/pi/wladio/venv
 python3.9 -m venv venv
 source venv/bin/activate
-pip3 install wheel cython
-pip3 install python-mpv luma.oled pi-rc522 adafruit-circuitpython-bme680 adafruit-circuitpython-bme280 pygobject requests xmltodict pi-ina219 evdev dbus-python
+pip3.9 install wheel cython
+pip3.9 install python-mpv luma.oled pi-rc522 adafruit-circuitpython-bme680 adafruit-circuitpython-bme280 pygobject requests xmltodict pi-ina219 evdev dbus-python
